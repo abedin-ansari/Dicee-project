d95653c165d8e2ceb0679f8c20c561d4cf5948c4
abedin-ansari <jainulansari579@gmail.com>
2024-02-07 14:09:31 +0530

    changes html css and JS

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-
-var randomDiceImg = "dice" + randomNumber + ".jpg";
-
-var randomImgSrc = "images/"+ randomDiceImg;
-
-var img1 = document.querySelectorAll("img")[0];
-
-img1.setAttribute("src", randomImgSrc);
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImgSrc2 = "images/dice" + randomNumber2 + ".jpg";
-
-var img2 = document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
-
-if(randomNumber > randomNumber2)
-{
-    document.querySelector("h1").innerHTML = "Player 1 wins🏆";
-}
-else if(randomNumber < randomNumber2)
-{
-    document.querySelector("h1").innerHTML = "Player 2 wins🏆";
-}
-else{
-    document.querySelector("h1").innerHTML = "Draw!";
-}
-
+document.getElementById("rollButton").addEventListener("click", function() {
+    
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImg = "dice" + randomNumber + ".jpg";
+    var randomImgSrc = "images/" + randomDiceImg;
+    var img1 = document.querySelector(".img1");
+    img1.setAttribute("src", randomImgSrc);
+  
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomImgSrc2 = "images/dice" + randomNumber2 + ".jpg";
+    var img2 = document.querySelector(".img2");
+    img2.setAttribute("src", randomImgSrc2);
+  
+    
+    if (randomNumber > randomNumber2) {
+      document.querySelector("h1").innerHTML = "Player 1 wins🏆";
+    } else if (randomNumber < randomNumber2) {
+      document.querySelector("h1").innerHTML = "Player 2 wins🏆";
+    } else {
+      document.querySelector("h1").innerHTML = "Draw!";
+    }
+  });
+  
