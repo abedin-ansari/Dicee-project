7b269aac2e40f48da4e8650a8f90952077839d9b
abedin-ansari <jainulansari579@gmail.com>
2024-02-07 14:17:07 +0530

    js change

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-document.getElementById("rollButton").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function() {
     
+    var initialImgSrc1 = "images/dice6.jpg";
+    var initialImgSrc2 = "images/dice6.png";
+    
+    document.querySelector(".img1").setAttribute("src", initialImgSrc1);
+    document.querySelector(".img2").setAttribute("src", initialImgSrc2);
+  });
+  
+  
+  document.getElementById("rollButton").addEventListener("click", function() {
     var randomNumber = Math.floor(Math.random() * 6) + 1;
     var randomDiceImg = "dice" + randomNumber + ".jpg";
     var randomImgSrc = "images/" + randomDiceImg;
@@ -11,7 +20,6 @@ document.getElementById("rollButton").addEventListener("click", function() {
     var img2 = document.querySelector(".img2");
     img2.setAttribute("src", randomImgSrc2);
   
-    
     if (randomNumber > randomNumber2) {
       document.querySelector("h1").innerHTML = "Player 1 wins🏆";
     } else if (randomNumber < randomNumber2) {
@@ -19,5 +27,28 @@ document.getElementById("rollButton").addEventListener("click", function() {
     } else {
       document.querySelector("h1").innerHTML = "Draw!";
     }
-  });
+  });  
+
+// document.getElementById("rollButton").addEventListener("click", function() {
+    
+//     var randomNumber = Math.floor(Math.random() * 6) + 1;
+//     var randomDiceImg = "dice" + randomNumber + ".jpg";
+//     var randomImgSrc = "images/" + randomDiceImg;
+//     var img1 = document.querySelector(".img1");
+//     img1.setAttribute("src", randomImgSrc);
+  
+//     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+//     var randomImgSrc2 = "images/dice" + randomNumber2 + ".jpg";
+//     var img2 = document.querySelector(".img2");
+//     img2.setAttribute("src", randomImgSrc2);
+  
+    
+//     if (randomNumber > randomNumber2) {
+//       document.querySelector("h1").innerHTML = "Player 1 wins🏆";
+//     } else if (randomNumber < randomNumber2) {
+//       document.querySelector("h1").innerHTML = "Player 2 wins🏆";
+//     } else {
+//       document.querySelector("h1").innerHTML = "Draw!";
+//     }
+//   });
   
